edbce65da5f6ac4a6bd464657d7a3e6e58f0b260
samilabud <samilabud@gmail.com>
2020-09-30 20:26:21 -0400

    fixing db

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const image = require("./controllers/image")
 
 const db = knex({
     client: 'pg',
-    connection: {
-      host : process.env.DATABASE_URL,
-      ssl: true
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
     }
 });  
 
