d1ab7cefe67618f7ea4842312b5a13a2ef5c0a02
samilabud <samilabud@gmail.com>
2020-10-03 18:55:12 -0400

    fixing conecction

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require("./controllers/image");
 
-console.log(process.env.DATABASE_URL);
+console.log("DB URL",process.env.DATABASE_URL);
 const db = knex({
     client: 'pg',
     connectionString: process.env.DATABASE_URL,
